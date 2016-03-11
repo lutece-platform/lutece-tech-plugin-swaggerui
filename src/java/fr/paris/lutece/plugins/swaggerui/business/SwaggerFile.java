1e434955530d7c043b9e0853ccc6d8f01dc61836
Adrien Duchemin <adrien.duchemin@paris.fr>
2016-03-11 14:54:45 +0100

    Search all swagger.json files in the website
    Search the swagger files
    Display thenm in a iframe with a selector

---
+++ src/java/fr/paris/lutece/plugins/swaggerui/business/SwaggerFile.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2002-2016, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */ 
+package fr.paris.lutece.plugins.swaggerui.business;
+
+import java.io.Serializable;
+
+/**
+ * This is the business class for the object SwaggerFile
+ */ 
+public class SwaggerFile implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+    private String _strPluginName;
+    private String _strVersion;
+    private String _strPath;
+
+    /**
+     * Returns the PluginName
+     * @return The PluginName
+     */
+    public String getPluginName( )
+    {
+        return _strPluginName;
+    }
+
+    /**
+     * Sets the PluginName
+     * @param strPluginName The PluginName
+     */ 
+    public void setPluginName( String strPluginName )
+    {
+        _strPluginName = strPluginName;
+    }
+    
+    /**
+     * Returns the Version
+     * @return The Version
+     */
+    public String getVersion( )
+    {
+        return _strVersion;
+    }
+
+    /**
+     * Sets the Version
+     * @param strVersion The Version
+     */ 
+    public void setVersion( String strVersion )
+    {
+        _strVersion = strVersion;
+    }
+    
+    /**
+     * Returns the Path
+     * @return The Path
+     */
+    public String getPath( )
+    {
+        return _strPath;
+    }
+
+    /**
+     * Sets the Path
+     * @param strPath The Path
+     */ 
+    public void setPath( String strPath )
+    {
+        _strPath = strPath;
+    }
+}
---
+++ src/java/fr/paris/lutece/plugins/swaggerui/service/SwaggerFileService.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2002-2016, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.swaggerui.service;
+
+import fr.paris.lutece.plugins.swaggerui.business.SwaggerFile;
+import fr.paris.lutece.portal.service.util.AppPathService;
+
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * This service searchs swagger.json files
+ */
+public class SwaggerFileService
+{
+    private static final String SWAGGER_DIRECTORY_NAME = "swagger";
+    private static final String SWAGGER_DIRECTORY_PATH = "/plugins";
+    
+    /**
+     * Returns the list of swagger files 
+     * @param request
+     * @return The list of swagger files
+     */
+    public static List<SwaggerFile> getSwaggerFiles( HttpServletRequest request )
+    {
+        List<SwaggerFile> listSwaggerFiles = new ArrayList<>();
+        List<File> listSwaggerDirectories = new ArrayList<>( );
+        String[] filesExtension = {"json"};
+        File folderWebApp = new File( AppPathService.getWebAppPath( ) + SWAGGER_DIRECTORY_PATH );
+        findDirectory( listSwaggerDirectories, folderWebApp );
+        
+        for ( File swaggerDirectory : listSwaggerDirectories )
+        {
+            Collection<File> filesSwagger = FileUtils.listFiles( swaggerDirectory, filesExtension, true);
+            for (File fileSwagger : filesSwagger)
+            {
+                SwaggerFile swaggerFile = new SwaggerFile();
+                swaggerFile.setPluginName( swaggerDirectory.getParentFile().getName() );
+                swaggerFile.setVersion( fileSwagger.getParentFile().getName() );
+                
+                String relativePath = new File(AppPathService.getWebAppPath( )).toURI().relativize(fileSwagger.toURI()).getPath();
+                swaggerFile.setPath( AppPathService.getBaseUrl( request ) + relativePath );
+                
+                listSwaggerFiles.add( swaggerFile );
+            }
+        }
+        return listSwaggerFiles;
+    }
+    
+    private static void findDirectory( List<File> listSwaggerDirectories, File parentDirectory )
+    {
+        for ( File file : parentDirectory.listFiles( ) )
+        {
+            if ( file.isFile( ) )
+            {
+                continue;
+            }
+            if (file.getName( ).equals( SWAGGER_DIRECTORY_NAME ) )
+            {
+                listSwaggerDirectories.add( file );
+            }
+            if(file.isDirectory( ) )
+            {
+               findDirectory( listSwaggerDirectories, file );
+            }
+        }
+    }
+}
